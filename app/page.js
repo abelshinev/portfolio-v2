5b116d23726d24e0c1979be34fe2794cf963faa2
Abel Varghese <abelshinevarghese@gmail.com>
2025-06-13 03:09:45 +0400

    fix: Working Parallax implemented

--- app/page.js
+++ app/page.js
@@ -1,20 +1,13 @@
 import Image from "next/image";
 import styles from "./page.module.css";
 import HomePage from "@/components/pages/Home";
+import TechStack from "@/components/pages/TechStack"
 
 export default function Home() {
   return (
     <div className="font-geistSans">
       <HomePage />
-      <div className="h-screen bg-zinc-800 bg-cover bg-center ">
-        <div className="h-full flex items-center justify-center text-white">
-          <h1 className="text-5xl font-bold">Parallax Background</h1>
-        </div>
-      </div>
-
-      <div className="h-screen bg-white text-black flex items-center justify-center">
-        <p>Scroll to see the parallax effect!</p>
-      </div>
+      <TechStack />
     </div>
   );
 }
--- components/pages/Home.js
+++ components/pages/Home.js
@@ -1,18 +1,37 @@
 "use client"
 import { Button } from "@/components/ui/button";
-import { motion } from "framer-motion";
+import { motion, useScroll, useTransform } from "framer-motion";
 import { LucideRocket } from "lucide-react";
 import FlipWordsDemo from "../flip-words-demo";
+import { useRef } from "react";
+import { useEffect } from "react";
+
 
 export default function HomePage() {
+
+  const ref = useRef(null);
+  const { scrollYProgress } = useScroll({ target: ref, offset: ["start end", "end start"] });
+  const y = useTransform(scrollYProgress, [0, 1], ["0%", "40%"]); // slower scroll = parallax
+
+  useEffect(() => {
+    scrollYProgress.on("change", (v) => console.log("Scroll:", v));
+  }, [scrollYProgress]);
+
   return (
-    <main className="min-h-screen bg-herobg bg-fixed bg-cover text-white flex flex-row items-center justify-center p-8 ">
-      <div className="absolute inset-0 backdrop-blur-sm bg-black/30 z-1"></div>
+    <motion.main 
+      ref={ref}
+      className="relative min-h-screen bg-herobg overflow-hidden text-white flex flex-row items-center justify-center p-8 "
+    >
+      <motion.div
+        style={{y}}
+        className="absolute inset-0 bg-herobg bg-cover bg-center z-0"
+      />
+      <div className="absolute inset-0 backdrop-blur-sm bg-black/30 z-10"/>
       <motion.div
         initial={{ opacity: 0, y: 40 }}
         animate={{ opacity: 1, y: 0 }}
         transition={{ duration: 0.8 }}
-        className="text-left space-y-4 w-[50vw] z-10"
+        className="text-left space-y-4 w-[50vw] z-20"
       >
         <h1 className="text-8xl font-bold"> Hi. I&apos;m Abel</h1>
         <FlipWordsDemo />
@@ -29,8 +48,8 @@ export default function HomePage() {
         </motion.div>
       </motion.div>
       <div className="z-10">
-        <div className="w-[360px] h-[60vh] bg-white border border-red-900 m-3 " />
+        <div className="w-[360px] h-[60vh] bg-white border m-3 " />
       </div>
-    </main>
+    </motion.main>
   );
 }
